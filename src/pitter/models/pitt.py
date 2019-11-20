7ad511f5413e4ffec6b8c43d6da1c3772761ca94
Alexander Kireev <alexander_k97@mail.ru>
2019-11-20 21:47:08 +0200

    add follower and pitt models and change names in structure puml

---
+++ src/pitter/models/pitt.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from django.db import models
+
+from pitter.models.base import BaseModel
+from pitter.models.user import User
+
+
+class Pitt(BaseModel):
+    owner = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    speech_audio_file = models.FileField()
+    speech_transcription = models.TextField()
