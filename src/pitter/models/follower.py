1a3002396ef833c45cda120a06930fe5913cafa1
Alexander Kireev <alexander_k97@mail.ru>
2019-12-04 00:19:26 +0200

    fix typo

--- src/pitter/models/follower.py
+++ src/pitter/models/follower.py
@@ -7,7 +7,6 @@ from pitter.models.user import User
 class Follower(BaseModel):
     target = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
     follower = models.ForeignKey(User, related_name='targets', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     @staticmethod
     def follow(target: User, follower: User):
